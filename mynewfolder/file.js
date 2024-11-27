808c73619fb948cc05910c2bda45f755bea0d702
UgyenChoden394 <ugyenchoden972@gmail.com>
2024-11-26 22:20:08 -0800

    initial commit

---
+++ mynewfolder/file.js
@@ -0,0 +1,13 @@
+function myfunction(){
+    x=3;
+    y=3;
+    z=x+y;
+}
+console.log(myfunction);
+
+function validateForm(){
+    var username=document.getElementById('username').value;
+    var usernamePattern = /^[a-zA-Z0-9_]{5,20}$/;
+
+}
+  
